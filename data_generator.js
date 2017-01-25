2661af6dfaa69303e05e04c3a14c1ccd6ddcfa7c
Andy Yeo <andy.yeo@gmail.com>
2017-01-25 18:24:57 +0800

    Increased upper case characters for first character in tweet.

--- data_generator.js
+++ data_generator.js
@@ -27,7 +27,7 @@ var randomElement = function(array){
 };
 
 // random tweet generator
-var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+var opening = ['Just', '', '', '', '', 'Ask me how i', 'Completely', 'Nearly', 'Productively', 'Efficiently', 'Last night I', 'The president', 'That wizard', 'A ninja', 'A seedy old man'];
 var verbs = ['downloaded', 'interfaced', 'deployed', 'developed', 'built', 'invented', 'experienced', 'navigated', 'aided', 'enjoyed', 'engineered', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
 var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
 var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
@@ -43,6 +43,7 @@ var generateRandomTweet = function(){
   tweet.user = randomElement(users);
   tweet.message = randomMessage();
   tweet.created_at = new Date();
+  console.log(tweet.created_at);
   addTweet(tweet);
 };
 
